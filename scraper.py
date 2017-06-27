e2b406475176391433ada7ebbdf304d780f8779b
Swapneel Mehta <swapneel.mehta96@gmail.com>
2017-06-27 15:56:13 +0200

    changed list-based operations to dict

--- scraper.py
+++ scraper.py
@@ -1,28 +1,41 @@
 #!/usr/bin/env/python
 import json, math, sys, datetime
 
-def getDistance(item, newData):
+def getDistance(item, newItem):
 	distance = 0.0
 	print("\nFinding distance")
-	print newData
+	print newItem
 	print item
 	# compare features originally present in the training set
-	for feature in range(len(max(item, newData, key=len))):
-		# keep first feature as day_of_the_week 
-		# and second as hour_of_the_day to calculate weight
-		# assume these are always present in newData
-		if feature <= 1:
-			distance += pow((float(item[feature]) - float(newData[feature])), 2)
+
+	# keep first feature as day_of_the_week 
+	# and second as hour_of_the_day to calculate weight
+	# assume these are always present in newItem
+	distance += pow((float(item["day_of_the_week"]) - float(newItem["day_of_the_week"])), 2)
+	distance += pow((float(item["hour_of_the_day"]) - float(newItem["hour_of_the_day"])), 2)		
+	
+	properties = ["client_user", "client_host", "client_program", "service_name"]
+	# if both have different lengths thus different features
+	for prop in properties:
+		if prop in item or prop in newItem:
+				try:
+					if item[prop] == newItem[prop]:
+						pass
+					else: 
+						distance += 1.0
+				
+				except:
+					distance += 1.0
+					# print "Error calculating distance"
 		else:
-			# if both have different lengths thus different features 
-			if len(item) != len(newData):
-				distance += 1.0
-			# if both have the same value for a feature
-			elif newData[feature] == item[feature]:
-				pass
-			# both are the same length but different value of features
-			else:
-				distance += 1.0
+			pass
+		pass
+	if "client_user" in item:
+		if "client_user" in newItem:
+			if item["client_user"] != newItem["client_user"] \
+			and distance <= 2.0:
+				distance += 30.0
+
 	finalDistance = math.sqrt(distance)
 	print "Distance : ", finalDistance
 	return finalDistance
@@ -31,8 +44,18 @@ def newConnection(items, newItem):
 	for item in items:
 		# match the ip address or the client user 
 		# in case ip is absent
-		if item[2] == newItem[2]:
-			return False
+		if "client_user" in items[item]:
+			if "client_user" in newItem:
+				if items[item]["client_user"] == newItem["client_user"]:
+					return False
+		elif "client_ip" in items[item]:
+			if "client_ip" in newItem:
+				if items[item]["client_ip"] == newItem["client_ip"]:
+					return False
+		elif "client_host" in items[item]:
+			if "client_host" in newItem:
+				if items[item]["client_host"] == newItem["client_host"]:
+					return False
 	return True
 
 
@@ -42,13 +65,13 @@ if __name__ == '__main__':
 		i = 0
 
 		# TODO: change list-based stuff to dict
-		data = []
-		anomalies = []
+		data = {}
+		anomalies = {}
 		distanceVote = []
 
 		# iterate over each logged line
 		for line in file:
-			newData = []
+			newItem = {}
 			#try:
 			json_data = json.loads(line)
 
@@ -59,65 +82,68 @@ if __name__ == '__main__':
 				day_of_the_week = datetime.datetime(int(timestamp[:4]), \
 					int(timestamp[5:7]), int(timestamp[8:10]), int(timestamp[11:13]), \
 					int(timestamp[14:16]), int(timestamp[17:19])).weekday()
-				newData.append(hour_of_the_day)
-				newData.append(day_of_the_week)
+				newItem["hour_of_the_day"] = hour_of_the_day
+				newItem["day_of_the_week"] = day_of_the_week
 				
 				if 'client_ip' in json_data['data']:
 					#print "\nclient_ip: ", json_data['data']['client_ip']
 					client_ip = str(json_data['data']['client_ip'])
-					newData.append(client_ip)
+					newItem["client_ip"] = client_ip
 
 				if 'client_user' in json_data['data']:
 					#print "\nclient_user: ", json_data['data']['client_user']
 					client_user = str(json_data['data']['client_user'])
-					newData.append(client_user)
+					newItem["client_user"] = client_user
 				
 				if 'client_host' in json_data['data']:
 					#print "\nclient_host: ", json_data['data']['client_host']
 					client_host = str(json_data['data']['client_host'])
-					newData.append(client_host)
+					newItem["client_host"] = client_host
 
 				if 'client_program' in json_data['data']:
 					#print "\nclient_program: ", json_data['data']['client_program']
 					client_program = str(json_data['data']['client_program'])
-					newData.append(client_program)
+					newItem["client_program"] = client_program
 
 				if 'service_name' in json_data['data']:
 					#print "\nservice_name: ", json_data['data']['service_name']
 					service_name = str(json_data['data']['service_name'])
-					newData.append(service_name)
+					newItem["service_name"] = service_name
 			
-			# print "\nNew Data :", newData
+			# print "\nNew Data :", newItem
 			
 			# ignore cases where data is incomplete
-			if len(newData) <= 2:
+			if len(newItem) <= 2:
 				continue
 
 			# for a new connection, blindly trust it
-			if newConnection(data, newData):
+			if newConnection(data, newItem):
 				print "\nNew item found"
 			
 			# voting for distance to all previously stored nodes	
 			for item in data:
-				distanceVote.append(getDistance(item, newData))
+				distance = getDistance(data[item], newItem)
+				distanceVote.append(distance)
+				if distance == 0.0:
+					print "*" * 20
 			# print "\nDistance Vote: ", distanceVote
 			distanceVote.sort()
 
 			if len(distanceVote) == 0:
 				print "\nNo votes for closest neighbour"
-				data.append(newData)
+				data[i] = newItem
 
-			elif distanceVote[0] <= 10.0:
+			elif distanceVote[0] <= 3.0:
 				# print(distanceVote)
 				print "\nMinimum distance: ", distanceVote[0]
-				data.append(newData)
+				data[i] = newItem
 
-			elif distanceVote[0] > 10.0:
-				print "\nAnomalous connection at: ", newData
-				anomalies.append(newData)
+			elif distanceVote[0] > 3.0:
+				print "\nAnomalous connection at: ", newItem
+				anomalies[i] = newItem
 			else:
 				print "\nError"
-				data.append(newData)
+				data[i] = newItem
 
 			#except:
 			#	print "\nError at line : ", i
